9fa9f666d593778dd3ef354fc6bec1b1c6cc22a1
vidur-simform <vidur@simformsolutions.com>
2023-03-31 10:20:10 +0530

    refactoring

--- main.js
+++ main.js
@@ -2,21 +2,23 @@ const scheduleArr = require('./data/schedule.json');
 const weekArray = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 const weekArrayIndex = {};
 
+//object of index for day
 weekArray.forEach((day, index) => {
     weekArrayIndex[day] = index;
 });
 
 //may change values for testing purpose
 //eg. 
-const curDate = new Date(2023,2,21,20,00,00); 
+// const curDate = new Date(2023, 2, 28, 22, 00, 00);
 
-// const curDate = new Date(); //current date
+const curDate = new Date(); //current date
 
 //get day in short format(eg. Mon)
 const curDay = curDate.toLocaleString("en-us", { weekday: "short" });
 
 const main = () => {
-    let status = 'Closed', i = 0, n = scheduleArr.length, diff = 0, ind = weekArrayIndex[scheduleArr[0].day], flag = false;
+    let status = 'Closed', i = 0, n = scheduleArr.length, diff = 0, ind = 0;
+    
     for (i = 0; i < n; i++) {
         if (scheduleArr[i].day == curDay) {
             const openTime = new Date(curDate.toLocaleDateString('en-US') + " " + scheduleArr[i].open).getTime();
@@ -27,26 +29,20 @@ const main = () => {
                 diff = (closeTime - curTime) / (60 * 60 * 1000);
                 break;
             } else if (curTime <= openTime) {
-                status = 'Closed';
-                diff = (openTime - curTime) / (60 * 60 * 1000);
+                ind = i;
                 break;
             }
-        } else if (!flag && weekArrayIndex[scheduleArr[i].day] > weekArrayIndex[curDay]) {
-            const nextOpenTime = new Date(curDate.toLocaleDateString('en-US') + " " + scheduleArr[i].open).getTime();
-            const curTime = curDate.getTime();
-            diff = (nextOpenTime - curTime)/(60*60*1000) + (weekArrayIndex[scheduleArr[i].day] - weekArrayIndex[curDay])*24;
-            flag = true;
+        } else if (weekArrayIndex[scheduleArr[i].day] > weekArrayIndex[curDay]) {
+            ind = i;
             break;
         }
     }
     if (status == 'Open') {
         console.log(`Open, The shop will be closed within ${diff.toFixed(2)} Hrs.`);
     } else {
-        if(!flag){
-            const nextOpenTime = new Date(curDate.toLocaleDateString('en-US') + " " + scheduleArr[ind].open).getTime();
-            const curTime = curDate.getTime();
-            diff = (nextOpenTime - curTime)/(60*60*1000) + (ind - weekArrayIndex[curDay] + 7)*24;
-        }
+        const nextOpenTime = new Date(curDate.toLocaleDateString('en-US') + " " + scheduleArr[ind].open).getTime();
+        const curTime = curDate.getTime();
+        diff = (nextOpenTime - curTime) / (60 * 60 * 1000) + ((weekArrayIndex[scheduleArr[ind].day] - weekArrayIndex[curDay] + 7) % 7) * 24;
         console.log(`Closed, The shop will be open after ${diff.toFixed(2)} Hrs.`);
     }
 };
